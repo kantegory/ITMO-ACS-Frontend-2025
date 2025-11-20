fd7a83fe56963e0647fc6253e4c27ae1d12a7234
Kristina Frolova <fr.kristina@inbox.ru>
2025-11-20 21:46:51 +0300

    [feat]: Добавлены материалы по ЛР1

---
+++ labs/K3439/Фролова Кристина/lr1/js/include.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const isLoggedIn = false;
+  const includeElements = document.querySelectorAll("[data-include]");
+  const promises = Array.from(includeElements).map(async (el) => {
+    const file = el.getAttribute("data-include");
+    if (!file) return;
+    const response = await fetch(file);
+    el.innerHTML = await response.text();
+  });
+
+  Promise.all(promises).then(() => {
+    const guestMenu = document.getElementById("guestMenu");
+    const userMenu = document.getElementById("userMenu");
+    if (!guestMenu || !userMenu) {
+      console.warn("guestMenu или userMenu не найдены в DOM");
+      return;
+    }
+    if (isLoggedIn) {
+      guestMenu.classList.add("d-none");
+      userMenu.classList.remove("d-none");
+    } else {
+      guestMenu.classList.remove("d-none");
+      userMenu.classList.add("d-none");
+    }
+  });
+});
