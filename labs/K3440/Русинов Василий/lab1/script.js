fcf9d6644affd4b6952d2a134e4fe619a032fd12
vendatt0r <vasyok03020454@gmail.com>
2025-11-26 17:54:21 +0300

    feat: some fixes

--- labs/K3440/Русинов Василий/lab1/script.js
+++ labs/K3440/Русинов Василий/lab1/script.js
@@ -34,3 +34,70 @@ document.getElementById("filterBtn")?.addEventListener("click", () => {
         item.style.display = ok ? "" : "none";
     });
 });
+document.getElementById("regBtn")?.addEventListener("click", () => {
+
+    let name = document.getElementById("regName").value.trim();
+    let email = document.getElementById("regEmail").value.trim();
+    let pass = document.getElementById("regPass").value;
+    let pass2 = document.getElementById("regPass2").value;
+
+    if (!name || !email || !pass) {
+        alert("Заполните все поля!");
+        return;
+    }
+
+    if (pass !== pass2) {
+        alert("Пароли не совпадают!");
+        return;
+    }
+
+    const user = {
+        name,
+        email,
+        pass
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Регистрация успешна!");
+    window.location.href = "login.html";
+});
+
+
+document.getElementById("loginBtn")?.addEventListener("click", () => {
+
+    let email = document.getElementById("loginEmail").value.trim();
+    let pass = document.getElementById("loginPass").value;
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!user) {
+        alert("Сначала зарегистрируйтесь!");
+        return;
+    }
+
+    if (email === user.email && pass === user.pass) {
+        localStorage.setItem("logged", "1");
+        window.location.href = "profile.html";
+    } else {
+        alert("Неверный email или пароль!");
+    }
+});
+
+
+if (window.location.pathname.includes("profile.html")) {
+
+    const logged = localStorage.getItem("logged");
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!logged || !user) {
+        window.location.href = "login.html";
+    } else {
+        document.getElementById("profileName").innerText = user.name;
+        document.getElementById("profileEmail").innerText = user.email;
+    }
+}
+
+document.getElementById("logoutBtn")?.addEventListener("click", () => {
+    localStorage.removeItem("logged");
+});
