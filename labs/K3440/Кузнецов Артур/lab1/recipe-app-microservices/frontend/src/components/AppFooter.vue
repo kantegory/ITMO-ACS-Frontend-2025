55a6add3fce1d5f7218be8e46fb38bb0598ae30a
Artur Kuznetsov <arturkuz.dkt@gmail.com>
2025-12-11 19:02:17 +0300

    feat: perform migration to vue.js

---
+++ labs/K3440/Кузнецов Артур/lab1/recipe-app-microservices/frontend/src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="text-center bg-light py-3 border-top" role="contentinfo">
+        <div class="container">
+            RecipeApp • Кузнецов Артур • K3440
+        </div>
+    </footer>
+</template>
