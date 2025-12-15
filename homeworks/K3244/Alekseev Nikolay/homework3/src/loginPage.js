6510e735b5534f1a58f500dc6ee1adc6d838d696
Nikolai Alexeev <N.Alexeev@seventest.ru>
2025-12-15 21:38:09 +0300

    copied files from hw2 to hw3

---
+++ homeworks/K3244/Alekseev Nikolay/homework3/src/loginPage.js
@@ -0,0 +1,16 @@
+import { users, saveUserToStorage } from "./data.js"
+
+const savedUser = localStorage.getItem("currentUser")
+if (savedUser) window.location.href = "index.html"
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const name = document.getElementById("loginName").value.trim()
+  if (!name) return
+  let user = users.find(u => u.name === name)
+  if (!user) {
+    alert("Пользователь не найден. Зарегистрируйтесь.")
+    return
+  }
+  saveUserToStorage(user)
+  window.location.href = "index.html"
+})
