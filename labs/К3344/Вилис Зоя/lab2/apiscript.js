a01ce1410501919e7b53ccdd8c88a604d1fa0862
spector-armstrong <franklinluvv@gmail.com>
2025-12-22 11:30:01 +0300

    added button events

--- labs/К3344/Вилис Зоя/lab2/apiscript.js
+++ labs/К3344/Вилис Зоя/lab2/apiscript.js
@@ -1,6 +1,7 @@
 const foodApi = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian";
 // const params = {category: "Vegetarian"};
 let extended = [];
+// const cards = document.querySelectorAll('.card');
 
 const container = document.querySelector('.gallery');
 
@@ -24,25 +25,46 @@ async function getItems (apiUrl) {
             // console.log(innerMeals[0].strMeal);
             extended.push({
                 recipe: innerMeals[0].strMeal,
-                skill: innerMeals[0].strInstructions.length
+                skill: innerMeals[0].strInstructions.length,
+                image: innerMeals[0].strMealThumb
             });
-
+        
+        meals.forEach((result) => {
             const card = document.createElement('div');
             card.classList = 'card-body';
 
             const content = `
                     <div class="card">
-                        <img src="${meal.strMealThumb}" class="card-img-top">
+                        <img src="${result.strMealThumb}" class="card-img-top">
                         <div class="card-body">
-                            <h4 class="card-title">${meal.strMeal}</h4>
-                            <p class="card-text">Click on this card to find the recipe for ${meal.strMeal}.</p>
+                            <h4 class="card-title">${result.strMeal}</h4>
+                            <p class="card-text">Click on this card to find the recipe for ${result.strMeal}.</p>
                             <p class="card-author">By <a href=profile.html>Venus</a></p>
                         </div>
                     </div>
             `;
 
             container.innerHTML += content;
-        }
+            });
+        };
+
+
+        //     const card = document.createElement('div');
+        //     card.classList = 'card-body';
+
+        //     const content = `
+        //             <div class="card">
+        //                 <img src="${meal.strMealThumb}" class="card-img-top">
+        //                 <div class="card-body">
+        //                     <h4 class="card-title">${meal.strMeal}</h4>
+        //                     <p class="card-text">Click on this card to find the recipe for ${meal.strMeal}.</p>
+        //                     <p class="card-author">By <a href=profile.html>Venus</a></p>
+        //                 </div>
+        //             </div>
+        //     `;
+
+        //     container.innerHTML += content;
+        // }
         
         // meals.forEach((result) => {
         //     // const extResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${result.idMeal}`)
@@ -62,46 +84,145 @@ async function getItems (apiUrl) {
 
         //     container.innerHTML += content;
         //     });
-
-        console.log(extended);
-
-        const extNumSorted = extended.toSorted((a, b) => a.skill - b.skill);
-
         const extAZSorted = extended.toSorted((a, b) => {
-            const nameA = a.recipe;
-            const nameB = b.recipe;
-            return nameA.localeCompare(nameB);
-        });
+                const nameA = a.recipe;
+                const nameB = b.recipe;
+                return nameA.localeCompare(nameB);
+            });
+        console.log(extAZSorted);
 
         const extZASorted = extended.toSorted((a, b) => {
-            const nameA = a.recipe;
-            const nameB = b.recipe;
-            return nameB.localeCompare(nameA);
-        });
-        
-        console.log(extNumSorted);
-        console.log(extAZSorted);
+                const nameA = a.recipe;
+                const nameB = b.recipe;
+                return nameB.localeCompare(nameA);
+            });
         console.log(extZASorted);
-        return responseJson;
+        
+        const extNumSorted = extended.toSorted((a, b) => a.skill - b.skill);
+
+        // let cards = Array.from(container.querySelectorAll('.card'));
+
+        document.querySelector(".sortAZ").addEventListener("click", function () {
+             extAZSorted.forEach((recipe) => {
+                const newCard = document.createElement('div');
+                newCard.classList = 'card-body';
+                
+                const content = `
+                        <div class="card">
+                            <img src="${recipe.strMealThumb}" class="card-img-top">
+                            <div class="card-body">
+                                <h4 class="card-title">${recipe.strMeal}</h4>
+                                <p class="card-text">Click on this card to find the recipe for ${recipe.strMeal}.</p>
+                                <p class="card-author">By <a href=profile.html>Venus</a></p>
+                            </div>
+                        </div>
+                `;
+
+                container.innerHTML += content;
+            })
+        });
+
+        document.querySelector(".sortZA").addEventListener("click", function () {
+            extZASorted.forEach((recipe) => {
+                const newCard = document.createElement('div');
+                newCard.classList = 'card-body';
+
+                const content = `
+                        <div class="card">
+                            <img src="${recipe.strMealThumb}" class="card-img-top">
+                            <div class="card-body">
+                                <h4 class="card-title">${recipe.strMeal}</h4>
+                                <p class="card-text">Click on this card to find the recipe for ${recipe.strMeal}.</p>
+                                <p class="card-author">By <a href=profile.html>Venus</a></p>
+                            </div>
+                        </div>
+                `;
+
+                container.innerHTML += content;
+            })
+        });
+
+        document.querySelector(".skill").addEventListener("click", function () {
+             extNumSorted.forEach((recipe) => {
+                const newCard = document.createElement('div');
+                newCard.classList = 'card-body';
+                
+                const content = `
+                        <div class="card">
+                            <img src="${recipe.strMealThumb}" class="card-img-top">
+                            <div class="card-body">
+                                <h4 class="card-title">${recipe.strMeal}</h4>
+                                <p class="card-text">Click on this card to find the recipe for ${recipe.strMeal}.</p>
+                                <p class="card-author">By <a href=profile.html>Venus</a></p>
+                            </div>
+                        </div>
+                `;
+
+                container.innerHTML += content;
+            })
+        });
+
+        console.log(extended);
+        return extended;
     // } catch (error) {
     //         console.error(`Caught error ${error}`)
     // };
 };
 
-
-
 getItems(foodApi);
 
-const sortAZ = function () {
-    pass;
-};
+// const sortAZ = function (foodApi) {
+//     getItems(foodApi).then(extended => {
+//         const extAZSorted = extended.toSorted((a, b) => {
+//                 const nameA = a.recipe;
+//                 const nameB = b.recipe;
+//                 return nameA.localeCompare(nameB);
+//             });
+//         console.log(extAZSorted);
+//         return extAZSorted;
+//     });
+// };
 
-const sortZA = function () {
-    pass;
-};
+// const sortZA = function () {
+//     getItems(foodApi).then(extended => {
+//         const extZASorted = extended.toSorted((a, b) => {
+//                 const nameA = a.recipe;
+//                 const nameB = b.recipe;
+//                 return nameB.localeCompare(nameA);
+//             });
+//         console.log(extZASorted);
+//         return extZASorted;
+//     });
+// };
 
-const sortSkill = function () {
-    pass;
-};
 
+// const sortSkill = function () {
+//     getItems(foodApi).then(extended => {
+//         const extNumSorted = extended.toSorted((a, b) => a.skill - b.skill);
+//         return extNumSorted;
+//     });
+//     console.log(extNumSorted);
+//     return extNumSorted;
+// };
 
+// // sortAZ(foodApi);
+// console.log(sortAZ(foodApi))
+
+// console.log(extAZSorted);
+// console.log(extZASorted);
+
+// document.querySelector(".sortZA").addEventListener("click", function () {
+//     getItems(foodApi).then(extended => {
+//         let cards = Array.from(container.querySelectorAll('.card'));
+//         const extZASorted = extended.toSorted((a, b) => {
+//                 const nameA = a.recipe;
+//                 const nameB = b.recipe;
+//                 return nameB.localeCompare(nameA);
+//             });
+//         console.log(extZASorted);
+//         return extZASorted;
+//     });
+//     cards.forEach((card) => {
+//         container.appendChild(card);
+//     });
+// })
