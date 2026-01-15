01a3ac36202efe1fc62439ac33682b828e1bdc1f
aleksandra <sashka_kolm@mail.ru>
2026-01-15 12:56:44 +0300

    removed footer

--- labs/K3342/Колмогорова Александра/lab3/src/App.vue
+++ labs/K3342/Колмогорова Александра/lab3/src/App.vue
@@ -1,24 +1,8 @@
 <template>
-  <div class="app-wrapper d-flex flex-column min-vh-100">
-    <main class="flex-grow-1">
+  <div> 
+    <main>
       <router-view />
     </main>
-    <Footer />
   </div>
 </template>
 
-<script>
-import Footer from './components/Footer.vue'
-
-export default {
-  components: {
-    Footer
-  }
-}
-</script>
-
-<style>
-.app-wrapper {
-  min-height: 100vh;
-}
-</style>
