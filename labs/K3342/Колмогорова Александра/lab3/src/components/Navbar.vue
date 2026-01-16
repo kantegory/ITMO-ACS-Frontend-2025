6f649e933af54fe4b86056ed5f72714c5fefbc35
aleksandra <sashka_kolm@mail.ru>
2026-01-16 19:57:36 +0300

    minor changes in navbar logic

--- labs/K3342/Колмогорова Александра/lab3/src/components/Navbar.vue
+++ labs/K3342/Колмогорова Александра/lab3/src/components/Navbar.vue
@@ -1,76 +1,10 @@
-<!-- <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-
-      <router-link
-        class="navbar-brand"
-        to="/"
-        @click="logout"
-      >
-        Let's cook
-      </router-link>
-
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link
-              to="/main"
-              class="nav-link"
-              active-class="active"
-            >
-              Home
-            </router-link>
-          </li>
-        </ul>
-
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <router-link
-              to="/account"
-              class="nav-link"
-              active-class="active"
-            >
-              Account
-            </router-link>
-          </li>
-        </ul>
-
-      </div>
-    </div>
-  </nav>
-</template>
-
-<script>
-import useAuth from '@/composables/useAuth'
-
-export default {
-    name: 'Navbar',
-
-    setup() {
-        const { logout } = useAuth()
-        return { logout }
-    }
-}
-</script> -->
-
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
 
       <router-link
         class="navbar-brand"
-        to="/"
-        @click="logout">
+        to="/">
         Let's cook
       </router-link>
 
@@ -164,11 +98,11 @@ import useAuth from '@/composables/useAuth'
 const route = useRoute()
 const { logout, isAuth } = useAuth()
 
-const isLoginPage = computed(() => route.path == '/login')
-const isRegisterPage = computed(() => route.path == '/register')
-const isIndexPage = computed(() => route.path == '/')
-const isAccountPage = computed(() => route.path == '/account')
-const isMainPage = computed(() => route.path == '/main')
+const isLoginPage = computed(() => route.name === 'login')
+const isRegisterPage = computed(() => route.name === 'register')
+const isIndexPage = computed(() => route.name === 'index')
+const isAccountPage = computed(() => route.name === 'account')
+const isMainPage = computed(() => route.name === 'main')
 const isRecipePage = computed(() => route.path.startsWith('/recipe/'))
 </script>
 
