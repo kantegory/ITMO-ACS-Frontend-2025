c7778c49730486848411b482c7fd974ee15f7d57
kr4nder <den-andronov@inbox.ru>
2026-01-19 01:16:41 +0300

    npm start,app.vue,index.html changes

--- labs/K3342/Андронов Денис/lab3/restorator-vue/src/App.vue
+++ labs/K3342/Андронов Денис/lab3/restorator-vue/src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <Navbar />
+    <div class="container py-4">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+// компонент навигации всегда виден сверху
+import Navbar from './components/Navbar.vue'
+</script>
+
+<style>
+/* стили подключаются позже */
+</style>
