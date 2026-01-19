fa302d5f7fe0c6e9eb3f00ef4ed467a89bddcd72
AriaTod <144653662+AriaTod@users.noreply.github.com>
2026-01-19 14:39:54 +0300

    +Bootstrap and style.css +header layout
    - Добавлен Bootstrap и перенесены стили из ЛР2
    - Обновлен header layout
    - задел на картинки

--- labs/components/AppHeader.vue
+++ labs/components/AppHeader.vue
@@ -1,23 +1,35 @@
 <template>
-  <header class="navbar navbar-expand navbar-dark bg-dark px-3">
-    <RouterLink to="/" class="navbar-brand">
-      Recipes
-    </RouterLink>
+  <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+    <div class="container">
+      <RouterLink class="navbar-brand" to="/">Рецепты</RouterLink>
 
-    <nav class="navbar-nav ms-auto">
-      <RouterLink to="/" class="nav-link">
-        Главная
-      </RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <RouterLink to="/search" class="nav-link">
-        Поиск
-      </RouterLink>
+      <div class="collapse navbar-collapse" id="mainNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">Главная</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/search">Поиск</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/profile">Кабинет</RouterLink>
+          </li>
+        </ul>
 
-      <RouterLink to="/profile" class="nav-link">
-        Профиль
-      </RouterLink>
-    </nav>
-  </header>
+        <div class="d-flex align-items-center">
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script setup>
